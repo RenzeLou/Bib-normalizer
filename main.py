b6f8bc326f3568c3470f80650bfb3eab1a48aa8b
Reza <marionojump0722@gmail.com>
2022-07-10 20:37:10 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+'''
+capitalize the title
+remove all url and doi
+'''
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input',type=str,default="./anthology.bib")
+parser.add_argument('--output',type=str,default="./anthology_cap.bib")
+parser.add_argument('--verbose',action="store_true")
+args = parser.parse_args()
+
+no_cap = ["with","of","for","to","from","and","on","in","under","a","by","the"]
+new_bib = ""
+
+def upper_already_cap(token:str):
+    new_token = ''
+    for t in token:
+        if t.isupper():
+            new_token += '{' + t + '}'
+        else:
+            new_token += t
+
+    return new_token
+
+def upper_all_tokens(title:str):
+    all_tokens = title.split(" ")
+    new_tokens = []
+    for i,tk in enumerate(all_tokens):
+        if i == 0 or tk.lower() not in no_cap:
+            ## must capitalize
+            tk = tk.replace("{","")
+            tk = tk.replace("}","")
+            new_tk = '{' + tk[0].upper() + '}' + upper_already_cap(tk[1:])
+            new_tokens.append(new_tk)
+        else:
+            new_tokens.append(tk)
+    
+    return " ".join(new_tokens)
+
+with open(args.input,"r",encoding="utf-8") as f:
+    ori_bib = f.readlines()
+    
+for line in ori_bib:
+    new_line = None
+    if 'url' in line or 'doi' in line or 'publisher' in line:
+        new_line = ""  ## remove
+    elif ('title=' in line or 'title =' in line) and 'booktitle' not in line:
+        start = line.index('{')  ## the first position of '{'
+        end = len(line) - line[::-1].index("}")  ## the last position of '}'
+        title = line[start+1:end-1]
+        left,right = line[:start+1],line[end-1:]
+        new_title = upper_all_tokens(title)
+        new_line = left + new_title + right
+    else:
+        new_line = line
+    new_bib += new_line
+
+if not args.verbose:
+    print(new_bib)
+
+with open(args.output,"w",encoding="utf-8") as f:
+    f.write(new_bib)
+
+print("write capitalized bib file at",args.output)
